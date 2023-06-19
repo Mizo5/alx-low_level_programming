d11b5824733f14f11b29ab84428a51d1a3927ba1
Hamza <hamzaait2@gmail.com>
2023-06-18 17:33:58 -0700

    array range

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -33,12 +33,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (size == 0 || nmemb == 0)
 		return (NULL);
 
-	m = malloc(sizeof(int) * nmemb);
+	m = malloc(nmemb * size);
 
 	if (m == NULL)
 		return (NULL);
 
-	_memset(m, 0, sizeof(int) * nmemb);
+	_memset(m, 0, nmemb * size);
 
 	return (m);
 }
